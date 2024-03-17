5016c9d8b8cb2177403f66a2f4b9a183b924a88c
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-03-17 12:53:25 +0100

    set middlewares on booking routes

--- src/routes/bookings.routes.ts
+++ src/routes/bookings.routes.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import { BookingController } from "../controllers/BookingController";
+import { isAgent } from "../middlewares/isAgent";
+import { auth } from "../middlewares/auth";
+import { isSuperAdmin } from "../middlewares/isSuperAdmin";
 
 const router = express.Router();
 const bookingController = new BookingController();
 
-router.post("/newbooking", bookingController.createBooking);
-router.post("/newcruise", bookingController.createCruise);
-router.get("/mybookings/:id", bookingController.getMyBookings);
-router.get("/allbookings", bookingController.getAllBookings);
-router.delete("/:id", bookingController.deleteBooking);
+router.post("/newbooking", auth, isAgent, bookingController.createBooking);
+router.post("/newcruise", auth, isAgent, bookingController.createCruise);
+router.get("/mybookings/:id", auth, bookingController.getMyBookings);
+router.get("/allbookings", auth, isSuperAdmin, bookingController.getAllBookings);
+router.delete("/:id", auth, isSuperAdmin, bookingController.deleteBooking);
 
 
 export default router;
