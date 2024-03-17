f4756f376640ac5a7d8b10697101c88c19c75a22
GabrielEscudillo <gabrielescudillo@gmail.com>
2024-03-17 18:28:08 +0100

    removed unused seeders

--- src/database/seeders/DatabaseSeeder.ts
+++ src/database/seeders/DatabaseSeeder.ts
@@ -1,14 +1,10 @@
-// import { roleSeeder } from "./RoleSeeder";
 import { userSeeder } from "./UserSeeder";
-// import { agentSeeder } from "./AgentSeeder";
 
 
 // -----------------------------------------------------------------------------
 
 (async() => {
-    // await roleSeeder();
     await userSeeder();
-    // await agentSeeder();
     
 })();
 
